554e583324992f0fb8d4e6c370f5b912d69d98e1
shapiraoz <shapiraoz@gmail.com>
2018-08-24 22:48:53 +0300

    add basicDataStructres

---
+++ basicDataStructers.py
@@ -0,0 +1,216 @@
+
+
+class basicDsBaseArr:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
+
+
+class Stack(basicDsBaseArr):
+
+    def __init__(self):
+        basicDsBaseArr.__init__()
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+
+class Queue (basicDsBaseArr):
+
+    def __init__(self):
+        basicDsBaseArr.__init__()
+
+    def enqueue(self, item):
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+
+class Deque (basicDsBaseArr):
+
+    def __init__(self):
+        basicDsBaseArr.__init__()
+
+    def addFront(self, item):
+        self.items.append(item)
+
+    def addRear(self, item):
+        self.items.insert(0,item)
+
+    def removeFront(self):
+        return self.items.pop()
+
+    def removeRear(self):
+        return self.items.pop(0)
+
+
+
+class Node:
+    def __init__(self,initdata):
+        self.data = initdata
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self,newdata):
+        self.data = newdata
+
+    def setNext(self,newnext):
+        self.next = newnext
+
+
+
+class AbstractList:
+
+    def __init__(self):
+        self.head=Node
+
+    def isEmpty(self):
+        return self.head is None
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current is not None:
+            count = count + 1
+            current = current.getNext()
+
+        return count
+
+
+class UnorderedList(AbstractList):
+
+    def __init__(self):
+        AbstractList.__init__()
+        self.head = None
+
+
+    def add(self, item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current is not None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+
+        return found
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        if previous is None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+
+
+class OrderedList (AbstractList):
+    def __init__(self):
+        AbstractList.__init__()
+
+    def add(self,item):
+        temp = Node(item)
+        found =False
+
+        if self.head is None:
+            self.head = temp
+
+        current = self.head
+        previous = self.head
+
+        while current is not None and not found:
+            if current.getData() <= item:
+                temp.next = current.next
+                previous.next = temp
+
+            else:
+                previous = current
+                current = current.next
+
+
+class HashTable:
+    def __init__(self):
+        self.size = 11
+        self.slots = [None] * self.size
+        self.data = [None] * self.size
+
+    def put(self, key, data):
+        hashvalue = self.hashfunction(key, len(self.slots))
+
+        if self.slots[hashvalue] is None:
+            self.slots[hashvalue] = key
+            self.data[hashvalue] = data
+        else:
+            if self.slots[hashvalue] == key:
+                self.data[hashvalue] = data  # replace
+            else:
+                nextslot = self.rehash(hashvalue, len(self.slots))
+                while self.slots[nextslot] is not None and \
+                        self.slots[nextslot] != key:
+                    nextslot = self.rehash(nextslot, len(self.slots))
+
+                if self.slots[nextslot] is None:
+                    self.slots[nextslot] = key
+                    self.data[nextslot] = data
+                else:
+                    self.data[nextslot] = data  # replace
+
+    def hashfunction(self, key, size):
+        return key % size
+
+    def rehash(self, oldhash, size):
+        return (oldhash + 1) % size
+
+    def get(self, key):
+        startslot = self.hashfunction(key, len(self.slots))
+
+        data = None
+        stop = False
+        found = False
+        position = startslot
+        while self.slots[position] != None and \
+                not found and not stop:
+            if self.slots[position] == key:
+                found = True
+                data = self.data[position]
+            else:
+                position = self.rehash(position, len(self.slots))
+                if position == startslot:
+                    stop = True
+        return data
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __setitem__(self, key, data):
+        self.put(key, data)
